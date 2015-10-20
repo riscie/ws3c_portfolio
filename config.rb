a92a4fdc939f8cbf165eaf20589cca13ba5d0c3b
Martin Brack <martin.brack@students.fhnw.ch>
2015-10-20 12:43:31 +0200

    Added compass-normalize and minify settings in config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 require 'bootstrap-sass'
 require 'compass/import-once/activate'
+require 'compass-normalize'
 # Require any additional compass plugins here.
 
 
@@ -11,7 +12,7 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
