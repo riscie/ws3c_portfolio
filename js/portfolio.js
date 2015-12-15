497da3a39a27dd164b9a33cef155b0ded8f8db48
Martin Brack <martin.brack@students.fhnw.ch>
2015-12-15 13:20:57 +0100

    implemented header section

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,9 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $('.button-collapse').sideNav({
+      edge: 'right'      
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
