fb360a106828cf42a9100f6467f37529675050e9
Martin Brack <martin.brack@students.fhnw.ch>
2015-12-18 22:14:36 +0100

    animated skill bars with growing instead of fadeIns

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,15 +5,39 @@
       edge: 'right'      
     });
     
-    var waypoints = $('.animated').waypoint({
+    $('.animated').waypoint({
       handler: function(direction) {
         var element = $(this.element);
         element.addClass(element.data('animation'));
       },
         offset: '80%'
     });
+    
+    $('.barPercentage').waypoint({
+      handler: function(direction) {
+        var element = $(this.element);
+        element.css("width", element.data('width') + "%");
+        countUp(this.element);
+        this.destroy();
+      },
+        offset: '90%'
+    });
 
   }); // end of document ready
+  
+  // counter function for skill bars
+  function countUp(element) {
+    $(element).prop('Counter',0).animate({
+        Counter: $(element).data('width')
+    }, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+            $(element).find('span').html(Math.ceil(now) + "%");
+        }
+    });
+  } // end of function countUp
+  
 })(jQuery); // end of jQuery name space
 
 
