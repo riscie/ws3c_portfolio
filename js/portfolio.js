a2f93f7a24a6421a59a81a24c6f3274a7703a98f
Martin Brack <martin.brack@students.fhnw.ch>
2015-12-18 22:30:09 +0100

    added scrolling function for scrolling to

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,6 +4,14 @@
     $('.button-collapse').sideNav({
       edge: 'right'      
     });
+    
+    scrollTo();
 
   }); // end of document ready
+  
+  // Function for main menu. Scrolls to specific position
+  function scrollTo() {
+    $('a[href^=#]').on('click', function (e) { var href = $(this).attr('href'); $('html, body').animate({ scrollTop: $(href).offset().top -20 }, 'slow'); e.preventDefault(); });
+  }
+  
 })(jQuery); // end of jQuery name space
